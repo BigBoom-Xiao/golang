aa09f06fdd4320eb4caae15af8292f38620a198c
BigBoom-Xiao <a5414994@outlook.com>
2016-11-26 10:16:32 +0800

    Signed-off-by: BigBoom-Xiao <a5414994@outlook.com>

--- guessNumber/main.go
+++ guessNumber/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	//这是一段来自sort.Search方法源码注释的代码，有修改，原来有点bug
+	//很好的体现了go方法的调用和写法
 	fmt.Println("Pick a number from 0 to 100.")
 	fmt.Printf("Your number is %d\n",
 		sort.Search(100, func(i int) bool {
